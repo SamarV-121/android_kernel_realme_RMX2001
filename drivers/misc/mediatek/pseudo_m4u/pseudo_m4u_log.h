ce7cc91cce277e8a49cab8518e16f5c28c37c951
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-02-21 12:51:24 +0530

    drivers: mtk-pseudo_m4u: Use debug printing
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: Ia473cb55ac027c5efa5de60e24126db4a428a257

--- drivers/misc/mediatek/pseudo_m4u/pseudo_m4u_log.h
+++ drivers/misc/mediatek/pseudo_m4u/pseudo_m4u_log.h
@@ -45,7 +45,7 @@ do { \
 #define M4UTRACE() \
 do { \
 	if (!m4u_log_to_uart) \
-		pr_debug("[PSEUDO] %s, %d\n", __func__, __LINE__); \
+		pr_notice("[PSEUDO] %s, %d\n", __func__, __LINE__); \
 } while (0)
 
 #define M4U_PRINT_SEQ(seq_file, fmt, args...) \
@@ -53,7 +53,7 @@ do { \
 		if (seq_file)\
 			seq_printf(seq_file, fmt, ##args);\
 		else\
-			pr_notice(fmt, ##args);\
+			pr_debug(fmt, ##args);\
 	} while (0)
 
 #endif
